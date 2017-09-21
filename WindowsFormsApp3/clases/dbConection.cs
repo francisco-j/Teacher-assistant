064005f6013eaf88484684615b11a0845bc9b450
Javier Fuentes <francisco.jft@hotmail.com>
2017-09-20 23:51:20 -0500

    agrege calse "dbConection"

---
+++ WindowsFormsApp3/clases/dbConection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+
+namespace WindowsFormsApp3.clases
+{
+    class dbConection
+    {
+
+        OleDbConnection conn = new OleDbConnection();
+        OleDbDataAdapter ada = new OleDbDataAdapter();
+        OleDbCommand cmd = new OleDbCommand();
+
+        private void Connect()
+        {
+            try
+            {
+                conn.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\...\teacher assistant.accdb;";
+                conn.Open();
+                if (conn.State == ConnectionState.Open)
+                    MessageBox.Show("Connected");
+                else
+                    MessageBox.Show("DisConnected");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+}
