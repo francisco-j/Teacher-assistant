9868617578d34b5b63bc60ae52a6ee8259ef9b67
Javier Fuentes <francisco.jft@hotmail.com>
2017-11-16 20:22:33 -0600

    modifique la base de datos

--- WindowsFormsApp3/Properties/Resources.Designer.cs
+++ WindowsFormsApp3/Properties/Resources.Designer.cs
@@ -229,5 +229,15 @@ namespace WindowsFormsApp3.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Menu {
+            get {
+                object obj = ResourceManager.GetObject("Menu", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
